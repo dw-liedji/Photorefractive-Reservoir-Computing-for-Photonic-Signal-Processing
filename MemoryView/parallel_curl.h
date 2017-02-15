b602964b57d383d55744391de49a7bc6b2aab612
stefano <stefano@stefano>
2017-02-15 22:58:59 +0100

    Updated.

---
+++ MemoryView/parallel_curl.h
@@ -0,0 +1,175 @@
+
+/*
+ *  parallel_curl.h
+ *
+ *  Created on: 22 dec 2016
+ *  Author: Stefano Ceccotti
+ *  Author: Tommaso Catuogno
+*/
+
+#ifndef _PARALLEL_CURL_H
+#define _PARALLEL_CURL_H
+
+#include <omp.h>
+
+#include "memory_view.h"
+
+using namespace algebra;
+
+template<typename T>
+class ParallelCurl
+{
+    public:
+        ParallelCurl() {}
+        
+        void curl_E_OPENMP( int Np, T* E, T* H, T* D, T* C, T sc, size_m CU, size_m M, size_m R, size_m CO )
+        {
+            MemoryView<T>* _E = new MemoryView<T>( E, { CU, M, R, CO } );
+            MemoryView<T>* _H = new MemoryView<T>( H, { CU, M, R, CO } );
+            MemoryView<T>* _D = new MemoryView<T>( D, { CU, M, R, CO } );
+            MemoryView<T>* _C = new MemoryView<T>( C, { CU, M, CO } );
+            
+            const size_m c       = CU/Np;
+            const size_m _offset = CU%Np;
+
+            #pragma omp parallel num_threads(Np)
+            {
+                const int x = omp_get_thread_num();
+                const int offset   = (x < _offset) ? 1 : _offset;
+                const bool last    = (x == Np-1);
+                const size_m start = (x * c) + offset;
+                const size_m stop  = last ? CU : (((x+1) * c+1) + offset);
+                
+                curl_E( _E->slice( { start, stop, 0, M, 0, R, 0, CO } ),
+                        _H->slice( { start, stop, 0, M, 0, R, 0, CO } ),
+                        _D->slice( { start, stop, 0, M, 0, R, 0, CO } ),
+                        _C->slice( { start, stop, 0, M, 0, CO } ),
+                        sc, last );
+            }
+        }
+        
+    private:
+        inline void curl_E( MemoryView<T>* IN, MemoryView<T>* OUT, MemoryView<T>* D, MemoryView<T>* curl, const T sc, const bool last )
+        {
+            MemoryView<T> ret( IN->getDimensions() );
+            if(last) {
+                // X-component.
+                *(ret[":,:-1,:,0"]) += (*(IN[0][":,1:,:,2"]) - IN[0][":,:-1,:,2"]);
+                *(ret[":, -1,:,0"]) -=    IN[0][":,-1,:,2"];
+                *(ret[":,:,:-1,0"]) -= (*(IN[0][":,:,1:,1"]) - IN[0][":,:,:-1,1"]);
+                *(ret[":,:, -1,0"]) +=    IN[0][":,:,-1,1"];
+                
+                // Y-component.
+                *(ret[":,:,:-1,1"]) += (*(IN[0][":,:,1:,0"]) - IN[0][":,:,:-1,0"]);
+                *(ret[":,:, -1,1"]) -=    IN[0][":,:,-1,0"];
+                *(ret[":-1,:,:,1"]) -= (*(IN[0]["1:,:,:,2"]) - IN[0][":-1,:,:,2"]);
+                *(ret[" -1,:,:,1"]) +=    IN[0]["-1,:,:,2"];
+                
+                // Z-component.
+                *(ret[":-1,:,:,2"]) += (*(IN[0]["1:,:,:,1"]) - IN[0][":-1,:,:,1"]);
+                *(ret[" -1,:,:,2"]) -=    IN[0]["-1,:,:,1"];
+                *(ret[":,:-1,:,2"]) -= (*(IN[0][":,1:,:,0"]) - IN[0][":,:-1,:,0"]);
+                *(ret[":, -1,:,2"]) +=    IN[0][":,-1,:,0"];
+                
+                *curl = ret[":,:,1,:"];
+                *OUT -= ret * ((*D) * sc);
+            }
+            else {
+                // X-component.
+                *(ret[":-1,:-1,:,0"]) += (*(IN[0][":-1,1:,:,2"]) - IN[0][":-1,:-1,:,2"]);
+                *(ret[":-1, -1,:,0"]) -=    IN[0][":-1,-1,:,2"];
+                *(ret[":-1,:,:-1,0"]) -= (*(IN[0][":-1,:,1:,1"]) - IN[0][":-1,:,:-1,1"]);
+                *(ret[":-1,:, -1,0"]) +=    IN[0][":-1,:,-1,1"];
+                
+                // Y-component.
+                *(ret[":-1,:,:-1,1"]) += (*(IN[0][":-1,:,1:,0"]) - IN[0][":-1,:,:-1,0"]);
+                *(ret[":-1,:, -1,1"]) -=    IN[0][":-1,:,-1,0"];
+                *(ret[":-1,:,:,  1"]) -= (*(IN[0]["1:, :, :,2"]) - IN[0][":-1,:,:  ,2"]);
+                
+                // Z-component.
+                *(ret[":-1,:,:,  2"]) += (*(IN[0]["1: , :,:,1"]) - IN[0][":-1,:  ,:,1"]);
+                *(ret[":-1,:-1,:,2"]) -= (*(IN[0][":-1,1:,:,0"]) - IN[0][":-1,:-1,:,0"]);
+                *(ret[":-1, -1,:,2"]) +=    IN[0][":-1,-1,:,0"];
+                
+                *(curl[0][":-1"]) = ret[":-1,:,1,:"];
+                *(OUT[0][":-1"]) += *ret[":-1"] * (*D[0][":-1"] * sc);
+            }
+        }
+        
+    public:
+        void curl_H_OPENMP( int Np, T* E, T* H, T* D, T* C, T sc, size_m CU, size_m M, size_m R, size_m CO )
+        {
+            MemoryView<T>* _E = new MemoryView<T>( E, { CU, M, R, CO } );
+            MemoryView<T>* _H = new MemoryView<T>( H, { CU, M, R, CO } );
+            MemoryView<T>* _D = new MemoryView<T>( D, { CU, M, R, CO } );
+            MemoryView<T>* _C = new MemoryView<T>( C, { CU, M, CO } );
+            
+            const size_m c       = CU/Np;
+            const size_m _offset = CU%Np;
+
+            #pragma omp parallel num_threads(Np)
+            {
+                const int x = omp_get_thread_num();
+                const int offset   = (x < _offset) ? 1 : _offset;
+                const bool first   = (x == 0);
+                const size_m start = (first) ? (x * c) : ((x * c) - 1) + offset;
+                const size_m stop  = (x == Np-1) ? CU : ((x+1)*c + offset);
+                
+                curl_H( _H->slice( { start, stop, 0, M, 0, R, 0, CO } ),
+                        _E->slice( { start, stop, 0, M, 0, R, 0, CO } ),
+                        _D->slice( { start, stop, 0, M, 0, R, 0, CO } ),
+                        _C->slice( { start, stop, 0, M, 0, CO } ),
+                        sc, first );
+            }
+        }
+        
+    private:
+        inline void curl_H( MemoryView<T>* IN, MemoryView<T>* OUT, MemoryView<T>* D, MemoryView<T>* curl, const T sc, const bool first )
+        {
+            MemoryView<T> ret( IN->getDimensions() );
+            if(first) {
+                // X-component.
+                *(ret[":,1:,:,0"]) += (*(IN[0][":,1:,:,2"]) - IN[0][":,:-1,:,2"]);
+                *(ret[":,0 ,:,0"]) +=    IN[0][":,0 ,:,2"];
+                *(ret[":,:,1:,0"]) -= (*(IN[0][":,:,1:,1"]) - IN[0][":,:,:-1,1"]);
+                *(ret[":,:,0 ,0"]) -=    IN[0][":,:,0 ,1"];
+                
+                // Y-component.
+                *(ret[":,:,1:,1"]) += (*(IN[0][":,:,1:,0"]) - IN[0][":,:,:-1,0"]);
+                *(ret[":,:,0 ,1"]) +=    IN[0][":,:,0 ,0"];
+                *(ret["1:,:,:,1"]) -= (*(IN[0]["1:,:,:,2"]) - IN[0][":-1,:,:,2"]);
+                *(ret["0 ,:,:,1"]) -=    IN[0]["0,:,:, 2"];
+                
+                // Z-component.
+                *(ret["1:,:,:,2"]) += (*(IN[0]["1:,:,:,1"]) - IN[0][":-1,:,:,1"]);
+                *(ret["0 ,:,:,2"]) +=    IN[0]["0 ,:,:,1"];
+                *(ret[":,1:,:,2"]) -= (*(IN[0][":,1:,:,0"]) - IN[0][":,:-1,:,0"]);
+                *(ret[":,0 ,:,2"]) -=    IN[0][":,0 ,:,0"];
+                
+                *curl = ret[":,:,1,:"];
+                *OUT += ret * ((*D) * sc);
+            }
+            else {
+                // X-component.
+                *(ret["1:,1:,:,0"]) += (*(IN[0]["1:,1:,:,2"]) - IN[0]["1:,:-1,:,2"]);
+                *(ret["1:,0 ,:,0"]) +=    IN[0]["1:,0 ,:,2"];
+                *(ret["1:,:,1:,0"]) -= (*(IN[0]["1:,:,1:,1"]) - IN[0]["1:,:,:-1,1"]);
+                *(ret["1:,:,0 ,0"]) -=    IN[0]["1:,:,0 ,1"];
+                
+                // Y-component.
+                *(ret["1:,:,1:,1"]) += (*(IN[0]["1:,:,1:,0"]) - IN[0]["1:,:,:-1,0"]);
+                *(ret["1:,:,0 ,1"]) +=    IN[0]["1:,:,0 ,0"];
+                *(ret["1:,:,:, 1"]) -= (*(IN[0]["1:,:,:, 2"]) - IN[0][":-1,:,:, 2"]);
+                
+                // Z-component.
+                *(ret["1:,:,:, 2"]) += (*(IN[0]["1:,:,:, 1"]) - IN[0][":-1,:,  :,1"]);
+                *(ret["1:,1:,:,2"]) -= (*(IN[0]["1:,1:,:,0"]) - IN[0]["1: ,:-1,:,0"]);
+                *(ret["1:,0 ,:,2"]) -=    IN[0]["1:,0 ,:,0"];
+                
+                *(curl[0]["1:"]) = ret["1:,:,1,:"];
+                *(OUT[0]["1:"]) += *ret["1:"] * (*D[0]["1:"] * sc);
+            }
+        }
+};
+
+#endif
--- MemoryView/parallel_curl.pyx
+++ MemoryView/parallel_curl.pyx
@@ -8,6 +8,7 @@ import multiprocessing
 from time import sleep
 import cython
 
+from cython import boundscheck, wraparound
 from cython cimport parallel
 from libc.stdio cimport printf
 from libc.stdlib cimport malloc, free
@@ -23,38 +24,14 @@ from libc.stdint cimport *
 ctypedef int64_t size_dim
 ctypedef int64_t size_m
 
-cdef extern from "ff_curl.h" namespace "ff_curl_parallel" nogil:
-    cdef cppclass FF_cube[T]:
-        FF_cube()
-        FF_cube( int )
-        void curl_E( int, MemoryView[T]*, MemoryView[T]*, MemoryView[T]*, MemoryView[T]*, T, size_m, size_m, size_m, size_m )
-        void curl_H( int, MemoryView[T]*, MemoryView[T]*, MemoryView[T]*, MemoryView[T]*, T, size_m, size_m, size_m, size_m )
-
-cdef extern from "memory_view.h" namespace "algebra" nogil:
-    cdef cppclass MemoryView[T]:
-        MemoryView( vector[size_dim] )
-        MemoryView( T*, vector[size_dim] )
-        #MemoryView( MemoryView[T]* )
-        
-        MemoryView[T]* slice( size_m, size_m, size_m, size_m, size_m, size_m, size_m, size_m )
-        MemoryView[T]* slice( vector[size_dim] )
-        
-        MemoryView[T]* operator[]( char* ) except +IndexError
-        
-        void curl_E( MemoryView[T]*, MemoryView[T]*, MemoryView[T]*, MemoryView[T]*, T, bool )
-        void curl_H( MemoryView[T]*, MemoryView[T]*, MemoryView[T]*, MemoryView[T]*, T, bool )
-        
-        void print_out()
-
+cdef extern from "parallel_curl.h" nogil:
+    cdef cppclass ParallelCurl[T]:
+        ParallelCurl()
+        void curl_E_OPENMP( int, T*, T*, T*, T*, T, size_m, size_m, size_m, size_m )
+        void curl_H_OPENMP( int, T*, T*, T*, T*, T, size_m, size_m, size_m, size_m )
 
 # Define the type of the elements.
 ctypedef float T
-
-cdef FF_cube[T]* ff = NULL
-
-def init( Np ):
-    global ff
-    ff = new FF_cube[T]( Np )
 
 
 def seq_curl_E(E): # Transforms an E field into an H field by performing a curl
@@ -85,118 +62,27 @@ def pySEQ_curl_E( E, H, D, sc ):
     E += sc * D * seq_curl_E( H )
 
 
-def curl_E( np.ndarray[T, ndim=4] E, np.ndarray[T, ndim=4] H, np.ndarray[T, ndim=4] D, np.ndarray[T, ndim=3] curl, sc, Np ):
-    
+@boundscheck(False)
+@wraparound(False)
+def curl_E( np.ndarray[T, ndim=4] E, np.ndarray[T, ndim=4] H, np.ndarray[T, ndim=4] D, np.ndarray[T, ndim=3] curl, T sc, int Np ):
     # Get the size of the input data structures.
     cdef size_m CU = H.shape[0], M = H.shape[1], R = H.shape[2], CO = H.shape[3]
-    
-    # 4D Vectors.
-    cdef MemoryView[T]* _E = new MemoryView[T]( &E[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _H = new MemoryView[T]( &H[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _D = new MemoryView[T]( &D[0,0,0,0], [CU, M, R, CO] )
-    
-    cdef MemoryView[T]* _curl = new MemoryView[T]( &curl[0,0,0], [CU, M, 1, CO] )
-    
-    # Partitions.
-    cdef int x, offset, last, start, stop
-    cdef int c = CU/Np
-    cdef int _offset = CU % Np
+    cdef T _sc = sc
     cdef int _Np = Np
-    cdef int _sc = sc
     
+    cdef ParallelCurl[T] pCurl
     with nogil:
-        # Parallel cycle with OpenMP.
-        for x in parallel.prange( _Np, schedule = 'static', num_threads = _Np ):
-            offset = 1 if x < _offset else _offset
-            last   = (x == _Np-1)
-            start  = 0 if x == 0 else ((x * c) + offset)
-            stop   = CU if last else ((x+1)*c + 1 + offset)
-            
-            _H.curl_E( _E.slice( start, stop, 0, M, 0, R, 0, CO ),
-                       _H.slice( start, stop, 0, M, 0, R, 0, CO ),
-                       _D.slice( start, stop, 0, M, 0, R, 0, CO ),
-                       _curl.slice( start, stop, 0, M, 0, 1, 0, CO ),
-                       _sc, last )
-        
-        free( _E ); free( _H ); free( _D );
-        free( _curl )
+        pCurl.curl_E_OPENMP( _Np, &E[0,0,0,0], &H[0,0,0,0], &D[0,0,0,0], &curl[0,0,0], _sc, CU, M, R, CO )
 
 
-def curl_H( np.ndarray[T, ndim=4] E, np.ndarray[T, ndim=4] H, np.ndarray[T, ndim=4] D, np.ndarray[T, ndim=3] curl, sc, Np ):
-    
+@boundscheck(False)
+@wraparound(False)
+def curl_H( np.ndarray[T, ndim=4] E, np.ndarray[T, ndim=4] H, np.ndarray[T, ndim=4] D, np.ndarray[T, ndim=3] curl, T sc, int Np ):
     # Get the size of the input data structures.
-    cdef int CU = H.shape[0], M = H.shape[1], R = H.shape[2], CO = H.shape[3]
-    
-    # 4D Vectors.
-    cdef MemoryView[T]* _E = new MemoryView[T]( &E[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _H = new MemoryView[T]( &H[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _D = new MemoryView[T]( &D[0,0,0,0], [CU, M, R, CO] )
-    
-    cdef MemoryView[T]* _curl = new MemoryView[T]( &curl[0,0,0], [CU, M, 1, CO] )
-    
-    # Partitions.
-    cdef int x, offset, first, last, start, stop
-    cdef int c = CU/Np, _offset = CU%Np
-    cdef int _Np = Np
-    cdef int _sc = sc
-    
-    
-    with nogil:
-        # Parallel cycle with OpenMP.
-        for x in parallel.prange( _Np, schedule = 'static', num_threads = _Np ):
-            offset = 1 if x < _offset else _offset
-            first  = (x == 0)
-            start  = 0 if first else ((x * c) - 1 + offset)
-            stop   = CU if (x == _Np-1) else ((x+1) * c + offset)
-            
-            _H.curl_H( _H.slice( start, stop, 0, M, 0, R, 0, CO ),
-                       _E.slice( start, stop, 0, M, 0, R, 0, CO ),
-                       _D.slice( start, stop, 0, M, 0, R, 0, CO ),
-                       _curl.slice( start, stop, 0, M, 0, 1, 0, CO ),
-                       _sc, first )
-        
-        free( _E ); free( _H ); free( _D );
-        free( _curl );
-
-def curl_E_FF( np.ndarray[T, ndim=4] E, np.ndarray[T, ndim=4] H, np.ndarray[T, ndim=4] D, np.ndarray[T, ndim=3] curl, sc, Np ):
-    
-    # Get the size of the input data structures.
-    cdef int CU = H.shape[0], M = H.shape[1], R = H.shape[2], CO = H.shape[3]
-    cdef int _sc = sc
+    cdef size_m CU = H.shape[0], M = H.shape[1], R = H.shape[2], CO = H.shape[3]
+    cdef T _sc = sc
     cdef int _Np = Np
     
-    # 4D Vectors.
-    cdef MemoryView[T]* _E = new MemoryView[T]( &E[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _H = new MemoryView[T]( &H[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _D = new MemoryView[T]( &D[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _curl = new MemoryView[T]( &curl[0,0,0], [CU, M, 1, CO] )
-    
-    # FastFlow object.
-    #cdef FF_cube[T]* ff = new FF_cube[T]( Np )
-    
+    cdef ParallelCurl[T] pCurl
     with nogil:
-        ff.curl_E( _Np, _E, _H, _D, _curl, _sc, CU, M, R, CO )
-        #del ff
-        free( _E ); free( _H ); free( _D ); free( _curl );
-
-
-def curl_H_FF( np.ndarray[T, ndim=4] E, np.ndarray[T, ndim=4] H, np.ndarray[T, ndim=4] D, np.ndarray[T, ndim=3] curl, sc, Np ):
-    
-    # Get the size of the input data structures.
-    cdef int CU = H.shape[0], M = H.shape[1], R = H.shape[2], CO = H.shape[3]
-    cdef int _sc = sc
-    cdef int _Np = Np
-    
-    # 4D Vectors.
-    cdef MemoryView[T]* _E = new MemoryView[T]( &E[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _H = new MemoryView[T]( &H[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _D = new MemoryView[T]( &D[0,0,0,0], [CU, M, R, CO] )
-    cdef MemoryView[T]* _curl = new MemoryView[T]( &curl[0,0,0], [CU, M, 1, CO] )
-    
-    # FastFlow object.
-    #cdef FF_cube[T]* ff = new FF_cube[T]( Np )
-    
-    with nogil:
-        ff.curl_H( _Np, _E, _H, _D, _curl, _sc, CU, M, R, CO )
-        #del ff
-        free( _E ); free( _H ); free( _D ); free( _curl );
+        pCurl.curl_H_OPENMP( _Np, &E[0,0,0,0], &H[0,0,0,0], &D[0,0,0,0], &curl[0,0,0], _sc, CU, M, R, CO )
